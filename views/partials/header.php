17fc7806f09cad1ee359e4ce69996fe193bad656
Théo Laroche <theo.laroche42300@gmail.com>
2026-02-17 14:57:22 +0100

    #4 fonction principal fini / version 1.0

--- views/partials/header.php
+++ views/partials/header.php
@@ -6,7 +6,7 @@
     <title>ComparOperator</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="../public/class.css" rel="stylesheet">
+    <link href="../../public/css/class.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
 </head>
 
